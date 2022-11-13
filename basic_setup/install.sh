9fe7e831650b4576346abed24bbb8e0f2d207ae9
Neil Armitage <neil.armitage@ensono.com>
2022-11-13 11:20:17 +0000

    Update install.sh

--- basic_setup/install.sh
+++ basic_setup/install.sh
@@ -22,6 +22,7 @@ sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
 
 sudo swapoff -a
+sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab   
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 
@@ -36,5 +37,4 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo kubeadm init --pod-network-cidr 10.1.0.0/16 --kubernetes-version 1.24.0
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-
-kubeadm join 192.168.0.201:6443 --token 3v4z5z.wbxj0pb2phqyxo6t --discovery-token-ca-cert-hash sha256:7bf7df82c4269cfecaf5fa48a16e31eb23e94a10446988b1d0135dab7337cead
+ 
